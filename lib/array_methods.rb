11f3c2a70582aede8ace09710de2cc7282c7af62
Firuz Afzalov <afzalov98@gmail.com>
2019-12-31 20:29:57 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,11 +10,8 @@ nil
 end
 
 def find_max_value(array)
-  counter = 0 
-  while array[counter] do
-    if
+  
 end
-find_max_value([1,2,1,3,4,3,5,4,3,2,1]))
 
 def find_min_value(array)
   # Add your solution here
